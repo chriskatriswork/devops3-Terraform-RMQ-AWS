41cbdd406291a22504b3a0c1c7a686e931621080
chriskatriswork <100373545+chriskatriswork@users.noreply.github.com>
2022-06-28 13:34:55 -0500

    Update main.tf

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "rmq" {
     ami = "ami-02d1e544b84bf7502"
     instance_type = "t2.micro"
     key_name = "rabbitmq"
-    vpc_security_group_ids = ["sg-048b10fe4954a3877 "]
+    vpc_security_group_ids = ["sg-048b10fe4954a3877"]
 
     tags = {
         Name = var.name
